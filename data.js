5430a8d3b8e76535974902e526fd47a7ed852d78
Vanessa Foerster <nessfoerster@gmail.com>
2023-01-27 13:04:07 -0500

    wip: adds one mask to data

---
+++ data.js
@@ -0,0 +1,9 @@
+module.exports = [
+    {
+      name: "The Bunny Hood", 
+      img: "https://oyster.ignimgs.com/mediawiki/apis.ign.com/the-legend-of-zelda-majoras-mask/c/cb/Bunny_Hood_%28Majora%27s_Mask%29.png", 
+      description: "The Bunny Hood is a Mask in Majora's Mask that lets the wearer run faster and jump farther than they normally would", 
+      price: 50, 
+      owned: false,
+    }
+]
