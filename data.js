50c924861b3521968c6d4e9b19008d327a7ba4dc
Vanessa Foerster <nessfoerster@gmail.com>
2023-01-28 10:18:17 -0500

    wip: adds more masks and data

--- data.js
+++ data.js
@@ -1,9 +1,31 @@
 module.exports = [
     {
-      name: "The Bunny Hood", 
-      img: "https://oyster.ignimgs.com/mediawiki/apis.ign.com/the-legend-of-zelda-majoras-mask/c/cb/Bunny_Hood_%28Majora%27s_Mask%29.png", 
-      description: "The Bunny Hood is a Mask in Majora's Mask that lets the wearer run faster and jump farther than they normally would", 
-      price: 50, 
-      owned: false,
+        name: "The Bunny Hood", 
+        img: "https://oyster.ignimgs.com/mediawiki/apis.ign.com/the-legend-of-zelda-majoras-mask/c/cb/Bunny_Hood_%28Majora%27s_Mask%29.png", 
+        description: "The Bunny Hood is a Mask in Majora's Mask that lets the wearer run faster and jump farther than they normally would", 
+        price: 50, 
+        owned: false,
+    },
+    {
+        name: "Keaton Mask",
+        img: "https://static.wikia.nocookie.net/zelda_gamepedia_en/images/1/19/MM3D_Keaton_Mask.png",
+        description: "The Keaton Mask is a Mask from Ocarina of Time and Majora's Mask. It is a mask that represents a Keaton, which is a popular childhood character in Ocarina of Time",
+        price: 10,
+        owned: false,
+    },
+    {
+        name: "Skull Mask",
+        img: "https://static.wikia.nocookie.net/zelda_gamepedia_en/images/2/2d/OoT_Skull_Mask_Render.png",
+        description: "The Skull Mask is an item in Ocarina of Time. It is a mask that was crafted to look like a monster and to make one look scary as well as tough.",
+        price: 20,
+        owned: false,
+    },
+    {
+        name: "Spooky Mask",
+        img: "https://static.wikia.nocookie.net/zelda_gamepedia_en/images/f/fb/OoT_Spooky_Mask_Render.png",
+        description: "The Spooky Mask can be worn by Link to receive new messages from people, notably by scaring them. It also possesses the unique side-effect of glowing in the dark at night.",
+        price: 30,
+        owned: false,
+
     }
 ]
