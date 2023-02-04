5a1213ad8bc5bedf0a5c3e2007f466b27482716a
Vanessa Foerster <nessfoerster@gmail.com>
2023-02-04 18:19:36 -0500

    wip: changes bunny hood img

--- data.js
+++ data.js
@@ -1,7 +1,7 @@
 module.exports = [
     {
         name: "The Bunny Hood", 
-        img: "https://oyster.ignimgs.com/mediawiki/apis.ign.com/the-legend-of-zelda-majoras-mask/c/cb/Bunny_Hood_%28Majora%27s_Mask%29.png", 
+        img: "https://static.wikia.nocookie.net/zelda_gamepedia_en/images/a/ad/MM3D_Bunny_Hood.png", 
         description: "The Bunny Hood is a Mask in Majora's Mask that lets the wearer run faster and jump farther than they normally would", 
         price: 50, 
         owned: false,
