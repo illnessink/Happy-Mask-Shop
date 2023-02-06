28bbe456ad56d3bb49551a7d845716021a85ea93
Vanessa Foerster <nessfoerster@gmail.com>
2023-02-06 11:55:14 -0500

    wip: adds two more masks

--- data.js
+++ data.js
@@ -2,7 +2,7 @@ module.exports = [
     {
         name: "The Bunny Hood", 
         img: "https://static.wikia.nocookie.net/zelda_gamepedia_en/images/a/ad/MM3D_Bunny_Hood.png", 
-        description: "The Bunny Hood is a Mask in Majora's Mask that lets the wearer run faster and jump farther than they normally would", 
+        description: "The Bunny Hood is a Mask in Majora's Mask that lets the wearer run faster and jump farther than they normally would.", 
         price: 50, 
         owned: false,
     },
@@ -31,7 +31,7 @@ module.exports = [
     {
         name: "Goron Mask",
         img: "https://static.wikia.nocookie.net/zelda_gamepedia_en/images/c/c1/MM3D_Goron_Mask_Artwork.png",
-        description: "The Goron Mask is a Mask from Ocarina of Time and Majora's Mask. It does not have much of a purpose in Ocarina of Time, but it is a useful and required item in Majora's Mask.",
+        description: "The Goron Mask is a Mask from Ocarina of Time and Majora's Mask. It does not have much of a purpose in Ocarina of Time, but it is a useful and required item in Majora's Mask. The mask turns Link into a Goron, complete with increased size and strength.",
         price: 500,
         owned: false,
     },
@@ -48,5 +48,19 @@ module.exports = [
         description: "Wearing the mask allows Link to speak to Gossip Stones to learn new things about Hyrule and its inhabitants. The Happy Mask Shop Owner mentions that the mask was passed down by the Sheikah.",
         price: 250,
         owned: false,
-    }
+    },
+    {
+        name: "Gerudo Mask", 
+        img: "https://static.wikia.nocookie.net/zelda_gamepedia_en/images/6/69/OoT_Gerudo_Mask_Render.png", 
+        description: "The Gerudo Mask is a mask obtainable in Ocarina of Time that looks like a Gerudo woman, being beautiful and exotic. Many people across Hyrule have different reactions to the mask when Link wears it. While some Hyruleans think that Link is a pretty, Gerudo woman, other people know that Link is wearing a mask.", 
+        price: 50, 
+        owned: false,
+    },
+    {
+        name: "Majoras Mask", 
+        img: "https://static.wikia.nocookie.net/zelda_gamepedia_en/images/3/3b/MM_Majora%27s_Mask_Render.png", 
+        description: "Majora's Mask is a powerful and evil mask. Once used in hexing rituals by an ancient tribe, the mask contains a seemingly sentient being that seeks to bring the Moon down on Termina in the events of the game Majora's Mask. However, little else is known about the mask. It is also not known where and how the Happy Mask Salesman acquired the mask.", 
+        price: 1000, 
+        owned: false,
+    },
 ]
